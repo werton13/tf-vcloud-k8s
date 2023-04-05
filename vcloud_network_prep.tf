7b58e06b811bd2ebae1bd7cddb264e21f22df876
werton13 <werton13@gmail.com>
2023-04-05 17:58:49 +0300

    fix load balancer for kube_api

--- vcloud_network_prep.tf
+++ vcloud_network_prep.tf
@@ -14,85 +14,85 @@ resource "vcd_vapp_org_network" "vappOrgNet" {
 }
 
 ###----- configure load balancer for a Kubernetes API-server---------------------------------------------------------------
-#resource "vcd_lb_app_profile" "kube_api" {
-#  org          = var.vcloud_orgname
-#  vdc          = var.vcloud_vdc
-#  edge_gateway = var.vcloud_edgegw
-#  enable_ssl_passthrough  = "true"
-#  persistence_mechanism   = "ssl-sessionid"
-#
-#  name = "http-app-profile"
-#  type = "https"
-#}
+resource "vcd_lb_app_profile" "kube_api" {
+  org          = var.vcloud_orgname
+  vdc          = var.vcloud_vdc
+  edge_gateway = var.vcloud_edgegw
+  enable_ssl_passthrough  = "true"
+  persistence_mechanism   = "ssl-sessionid"
 
-#resource "vcd_lb_service_monitor" "kube_api" {
-#  org          = var.vcloud_orgname
-#  vdc          = var.vcloud_vdc
-#  edge_gateway = var.vcloud_edgegw
-#
-#  name        = "kube_api"
-#  interval    = "10"
-#  timeout     = "15"
-#  max_retries = "3"
-#  type        = "https"
-#  expected    = "HTTP/1.0 200 OK"
-#  method      = "GET"
-#  url         = "/readyz"
-#  #send        = "{\"key\": \"value\"}"
-#  send        = "HTTP/2"
-#  receive     = "ok"
-#  #extension = {
-#  #  content-type = "application/json"
-#  #  linespan     = ""
-#  #}
-#}
+  name = "http-app-profile"
+  type = "https"
+}
 
-#resource "vcd_lb_server_pool" "kube_api_lb_pool" {
-#  org          = var.vcloud_orgname
-#  vdc          = var.vcloud_vdc
-#  edge_gateway = var.vcloud_edgegw
-#  depends_on  = [vcd_vapp.k8s_mgmt_vapp,
-#                 vcd_lb_service_monitor.kube_api]
-#
-#  name                 = "kube_api"
-#  description          = "description"
-#  algorithm            = "round-robin"
-#  enable_transparency  = false
-#  monitor_id = vcd_lb_service_monitor.kube_api.id
-#
-#    member {
-#    condition       = "enabled"
-#    name            = "${var.vms.masters.pref}-0"
-#    ip_address      = "${split("/", var.vms.masters.ip_pool[0])[0]}"
-#    port            = 6443
-#    monitor_port    = 6443
-#    weight          = 1
-#    min_connections = 0
-#    max_connections = 100
-#   }
-#    member {
-#    condition       = "drain"
-#    name            = "${var.vms.masters.pref}-1"
-#    ip_address      = "${split("/", var.vms.masters.ip_pool[1])[0]}"
-#    port            = 6443
-#    monitor_port    = 6443
-#    weight          = 1
-#    min_connections = 0
-#    max_connections = 100
-#   }
-#    member {
-#    condition       = "drain"
-#    name            = "${var.vms.masters.pref}-2"
-#    ip_address      = "${split("/", var.vms.masters.ip_pool[2])[0]}"
-#    port            = 6443
-#    monitor_port    = 6443
-#    weight          = 1
-#    min_connections = 0
-#    max_connections = 100
-#  }
-#
-#
-#}
+resource "vcd_lb_service_monitor" "kube_api" {
+  org          = var.vcloud_orgname
+  vdc          = var.vcloud_vdc
+  edge_gateway = var.vcloud_edgegw
+
+  name        = "kube_api"
+  interval    = "10"
+  timeout     = "15"
+  max_retries = "3"
+  type        = "https"
+  expected    = "HTTP/1.0 200 OK"
+  method      = "GET"
+  url         = "/readyz"
+  #send        = "{\"key\": \"value\"}"
+  send        = "HTTP/2"
+  receive     = "ok"
+  #extension = {
+  #  content-type = "application/json"
+  #  linespan     = ""
+  #}
+}
+
+resource "vcd_lb_server_pool" "kube_api_lb_pool" {
+  org          = var.vcloud_orgname
+  vdc          = var.vcloud_vdc
+  edge_gateway = var.vcloud_edgegw
+  depends_on  = [vcd_vapp.k8s_mgmt_vapp,
+                 vcd_lb_service_monitor.kube_api]
+
+  name                 = "kube_api"
+  description          = "description"
+  algorithm            = "round-robin"
+  enable_transparency  = false
+  monitor_id = vcd_lb_service_monitor.kube_api.id
+
+    member {
+    condition       = "enabled"
+    name            = "${var.vms.masters.pref}-0"
+    ip_address      = "${split("/", var.vms.masters.ip_pool[0])[0]}"
+    port            = 6443
+    monitor_port    = 6443
+    weight          = 1
+    min_connections = 0
+    max_connections = 100
+   }
+    member {
+    condition       = "drain"
+    name            = "${var.vms.masters.pref}-1"
+    ip_address      = "${split("/", var.vms.masters.ip_pool[1])[0]}"
+    port            = 6443
+    monitor_port    = 6443
+    weight          = 1
+    min_connections = 0
+    max_connections = 100
+   }
+    member {
+    condition       = "drain"
+    name            = "${var.vms.masters.pref}-2"
+    ip_address      = "${split("/", var.vms.masters.ip_pool[2])[0]}"
+    port            = 6443
+    monitor_port    = 6443
+    weight          = 1
+    min_connections = 0
+    max_connections = 100
+  }
+
+
+}
 
 #resource "vcd_lb_virtual_server" "kube_api_lb_vs" {
 #  depends_on    = [vcd_lb_server_pool.kube_api_lb_pool]
